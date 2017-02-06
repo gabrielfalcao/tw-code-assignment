9cbac1d915195c62214b82a54ee0efdb05bd56a1
Gabriel Falcão <gabriel@nacaolivre.org>
2017-02-06 00:50:24 -0500

    starting to design the TextParser

--- tests/unit/test_text_parser_engine.py
+++ tests/unit/test_text_parser_engine.py
@@ -1,3 +1,17 @@
 # -*- coding: utf-8 -*-
-
+from __future__ import unicode_literals
 from tw_conference_manager.engines import TextParser
+
+
+def test_parse_line():
+    "TextParser.parse_line() should receive a simple line as string and return structured metadata"
+
+    # Given an instance of TextParser
+    parser = TextParser()
+
+    # And a valid input in minutes
+    target = 'Writing Fast Tests Against Enterprise Rails 60min'
+    result = parser.parse_line(target)
+
+    # Then it should have returned a dictionary
+    result.should.be.a(dict)
